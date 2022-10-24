1ea09e1cd2f34fd0d1f303f06842324adf1ceb0f
Jeanjean <adrien.jeanjean@epsi.fr>
2022-10-24 11:27:03 +0200

    Error build

--- Java/src/test/java/com/gildedrose/GildRose.java
+++ Java/src/test/java/com/gildedrose/GildRose.java
@@ -3,8 +3,18 @@ package com.gildedrose;
 import java.io.*;
 
 public class GildRose {
-    public static void main(String[] args){
+    Product[] products;
 
-        System.out.println("bonjour tout le monde");
+    public GildRose(Product[] products){
+        this.products = products;
+    }
+    public void main(String[] args){
+        updateQuality();
+    }
+
+    public void updateQuality(){
+        for (int productCounter = 0; productCounter < this.products.length; productCounter++) {
+           this.products[productCounter].EndDay();
+        }
     }
 }
